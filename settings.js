5fab0ff74321eda7376e5a599d3f821ea8f7faf7
Michiel <michiel.fortuin@gmail.com>
2016-05-10 19:49:50 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,36 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('settings');
+ * mod.thing == 'a thing'; // true
+ */
+
+module.exports = {
+    miners: [1, 2],
+    maxCreeps: {
+        "repairder": 0,
+        "builder2": 0,
+        "builder": 0,
+        "upgrader": 0,
+        "harvester": 1,
+        "miner": 1,
+    },
+    creepTemplate:
+    {
+        "builder": "worker",
+        "builder2": "worker",
+        "harvester": "smallWorker",
+        "upgrader": "worker",
+        "miner": "miner",
+        "repairder": "repairder"
+    },
+    creepParts: {
+        "smallWorker": [WORK,CARRY,MOVE,MOVE],
+        "transporter": [CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE],
+        "worker": [WORK,WORK,WORK,CARRY,CARRY,MOVE,MOVE],
+        "repairder": [WORK,WORK,CARRY,MOVE,MOVE,MOVE],
+        "miner": [WORK,WORK,WORK,WORK,CARRY,MOVE]
+    }
+}
