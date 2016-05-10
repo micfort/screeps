cd57ad14140e7ad7320aba7b28701acf2df23466
Michiel <michiel.fortuin@gmail.com>
2016-05-10 20:34:05 +0200

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -3,21 +3,33 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep, targetIndex) {
 
-	    if(creep.memory.building && creep.carry.energy == 0) {
+	if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
             creep.memory.sourceTarget = undefined;
-	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	        creep.memory.sourceTarget = undefined;
-	    }
+	}
+	if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+		creep.memory.building = true;
+		creep.memory.sourceTarget = undefined;
+	}
 
         if(creep.memory.sourceTarget == undefined)
         {
             var sources = creep.room.find(FIND_STRUCTURES)
                                 .filter((s)=>{return s.structureType == STRUCTURE_CONTAINER;})
                                 .sort((s1, s2)=>{return s1.pos.getRangeTo(creep)-s2.pos.getRangeTo(creep);});            
-            creep.memory.sourceTarget = sources[0].id;
+            
+                        var sources = creep.room.find(FIND_STRUCTURES).filter((s)=>{return s.structureType == STRUCTURE_CONTAINER && _.sum(s.store) > 0;});
+            
+            if(sources.length > 0)
+            {
+                creep.memory.sourceTarget = sources[0].id;
+                creep.memory.sourceTargetType = STRUCTURE_CONTAINER;
+            }
+            else
+            {
+                var sources = creep.room.find(FIND_SOURCES).sort((s1, s1)=>{return s1.pos.getRangeTo(creep)-s2.pos.getRangeTo(creep);});
+                creep.memory.sourceTarget = sources[0].id;
+                creep.memory.sourceTargetType = 'mine';    
         }
 
 	   
@@ -41,11 +53,24 @@ var roleBuilder = {
             }
 	    }
 	    else {
-            var source = Game.getObjectById(creep.memory.sourceTarget);
-            if(source.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-            {
-                creep.moveTo(source);
-            }
+		var source = Game.getObjectById(creep.memory.sourceTarget);
+		var transferRC;
+		if(creep.memory.sourceTargetType == STRUCTURE_CONTAINER)
+		{
+			transferRC = source.transfer(creep, RESOURCE_ENERGY);
+		}
+		else
+		{
+			transferRC = creep.harvest(source);
+		}
+		if(transferRC == ERR_NOT_IN_RANGE)
+		{
+			creep.moveTo(source);
+		}
+		else if(transferRC == ERR_NOT_ENOUGH_RESOURCES)
+		{
+			creep.memory.sourceTarget = undefined;
+		}
 	    }
 	}
 };
